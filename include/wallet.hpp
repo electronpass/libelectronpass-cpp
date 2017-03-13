fcac9a13f2205aaf3dd169f936e55c421ed8e00f
Vid Drobnic <vid.drobnic@gmail.com>
2017-03-13 23:08:13 +0100

    defined wallet methods that need to be implemented

--- include/wallet.hpp
+++ include/wallet.hpp
@@ -21,6 +21,32 @@ along with libelectronpass.  If not, see <http://www.gnu.org/licenses/>.
 #include <vector>
 #include <string>
 
+/*
+ * TODO: Class methods to operate with data
+ * Wallet:
+ *   - constructor & deconstructor
+ *   - get_items (names only)
+ *   - set_items (override all items)
+ *   - get_item (get data for specific item)
+ *   - set_item (set data for specific items)
+ *   - add_item/s (add one or more items to the wallet)
+ *   - delete_item/s (delete one or more items to the wallet)
+ *
+ * Item:
+ *   - constructor & deconstructor
+ *   - variable name
+ *   - get_fields (names only)
+ *   - set fields (override all fields)
+ *   - get_field (get data for specific field)
+ *   - set_field (set data for specific field)
+ *   - add_field/s (add one or more fields to the item)
+ *   - delete_field/s (delete one or more fields from the item)
+ *
+ * Field:
+ *   - constructor & deconstructor
+ *   - variables/properties
+ */
+
 namespace electronpass {
     class Wallet {
       public:
