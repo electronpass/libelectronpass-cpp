06c2b81b683b671130c107c8bbcc89cb1a418213
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-03-19 14:32:33 +0000

    ignore prompt for package installation

--- .travis/install-sodium-trusty.sh
+++ .travis/install-sodium-trusty.sh
@@ -3,4 +3,4 @@
 sudo add-apt-repository ppa:chris-lea/libsodium;
 sudo echo "deb http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
 sudo echo "deb-src http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
-sudo apt-get update && sudo apt-get install libsodium-dev;
+sudo apt-get update && sudo apt-get install -y libsodium-dev;
