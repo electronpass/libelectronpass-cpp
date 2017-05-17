41d31c0c17ae3f24cca82cff24718ae93777f267
Vid Drobnic <vid.drobnic@protonmail.com>
2017-05-17 21:06:02 +0200

    removed iostream from wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -15,7 +15,6 @@ You should have received a copy of the GNU Lesser General Public License
 along with libelectronpass.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <iostream>
 #include "wallet.hpp"
 
 #define kFieldTypeUsername "username"
