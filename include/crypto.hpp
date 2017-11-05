6379e6a3c29f07b996ccb9541a5ff6ff717c4cdb
Matej Marinko <matejmarinko123@gmail.com>
2017-11-05 16:17:05 +0100

    nicer code, added comments

--- include/crypto.hpp
+++ include/crypto.hpp
@@ -28,7 +28,9 @@ along with libelectronpass.  If not, see <http://www.gnu.org/licenses/>.
 #include <string>
 #include <cassert>
 
+/// Size of key used for AES 256 encryption.
 #define KEY_SIZE CryptoPP::AES::MAX_KEYLENGTH
+/// Size of block used for AES 256 encryption.
 #define AES_BLOCKSIZE CryptoPP::AES::BLOCKSIZE
 
 /**
@@ -39,33 +41,37 @@ along with libelectronpass.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace electronpass {
-    //// All chars in Base64.
-    static const std::string BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    /// Salt used for AES key generation.
+    static const std::string KEY_SALT = "wwT>y\\x06f\\x19=\\x19RZt\\x11hpoy\\x052 H?\\x1b\\x0eM.\\n\\x000~([\\x17^\\x03ER,\\x15K:\\x1b)\\x01\\x01`{g\\x10\\x0eX\\x1e\\x18\\x14\\x12\\x02G\\t+\\x1d|Wz";
 
-    static const std::string KEY_SALT = "electronpass salt";  // TODO: set salt
-
-    /// Class for cryptographics functions and helper functions.
+    /// Class for cryptographic functions and helper functions.
     class Crypto {
-      private:
+    private:
         // Key for encryption
         byte key[KEY_SIZE];
-      public:
+    public:
 
         /**
          * @brief Constructor
          * @param password Password, from which encryption key will be generated.
          */
-        Crypto(std::string password);
+        Crypto(const std::string &password);
 
         /**
          * @brief Encrypts plain text.
          * We use AES256 authenticated encryption algorithm, implemented in library crypto++.
          * @param plain_text String, which will be encrypted.
-         * @param success If encryption was successful. See also check().
-         * @return Encrypted plain text, already converted to Base64. If encryption wasn't successful
-         * returns empty string.
+         * @return Encrypted plain text, already converted to Base64.
          */
-        std::string encrypt(const std::string& plain_text, bool& success) const;
+        std::string encrypt(const std::string &plain_text) const;
+
+        /**
+         * @deprecated Encrypts plain text.
+         * @param plain_text String, which will be encrypted.
+         * @param success parameter, used only for backwards compatibility (can be omitted). Should be always true.
+         * @return Encrypted plain text, already converted to Base64.
+         */
+        std::string encrypt(const std::string &plain_text, bool &success) const;
 
         /**
          * @brief Decrypts Base64 encoded cipher text.
@@ -74,10 +80,10 @@ namespace electronpass {
          * @param success True if message was decoded from cipher text, false otherwise.
          * @return Decrypted text if decrypting was successful. If not, empty string ("").
          */
-        std::string decrypt(const std::string& cipher_text, bool& success) const;
+        std::string decrypt(const std::string &cipher_text, bool &success) const;
 
         /**
-         * @brief Generate UUID used for identifying items.
+         * @brief Generate random UUID used for identifying items.
          * @return UUID
          */
         static std::string generate_uuid();
--- src/crypto.cpp
+++ src/crypto.cpp
@@ -17,15 +17,17 @@ along with libelectronpass.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "crypto.hpp"
 
-electronpass::Crypto::Crypto(std::string password) {
+electronpass::Crypto::Crypto(const std::string &password) {
+    // Generate key from password using SHA256 hash.
     CryptoPP::SHA256 sha256;
     CryptoPP::StringSource string_source(password + KEY_SALT, true,
                                          new CryptoPP::HashFilter(sha256,
                                                                   new CryptoPP::ArraySink(key,
-                                                                                          KEY_SIZE)));
+                                                                                          static_cast<size_t>(KEY_SIZE))));
 }
 
-std::string electronpass::Crypto::encrypt(const std::string &plain_text, bool &success) const {
+std::string electronpass::Crypto::encrypt(const std::string &plain_text) const {
+    // Generate random IV.
     CryptoPP::AutoSeededRandomPool random_pool;
     byte iv[AES_BLOCKSIZE];
     random_pool.GenerateBlock(iv, sizeof(iv));
@@ -33,14 +35,17 @@ std::string electronpass::Crypto::encrypt(const std::string &plain_text, bool &s
     CryptoPP::CBC_Mode<CryptoPP::AES>::Encryption encryption;
     encryption.SetKeyWithIV(key, static_cast<size_t >(KEY_SIZE), iv);
 
+    // Allocate memory for cipher text
     unsigned long encrypted_size = (plain_text.size() / AES_BLOCKSIZE + 1) * AES_BLOCKSIZE;
     auto encrypted_bytes = new byte[encrypted_size];  // Can't allocate on stack because of the standard
 
+    // Put plain text into encryption algorithm.
     CryptoPP::StringSource source(plain_text, true,
                                   new CryptoPP::StreamTransformationFilter(encryption,
                                                                            new CryptoPP::ArraySink(encrypted_bytes,
                                                                                                    encrypted_size),
                                                                            CryptoPP::StreamTransformationFilter::PKCS_PADDING));
+    // Generate signature (hash for authentication).
     CryptoPP::SHA256 sha256;
 
     CryptoPP::HashFilter hash_filter(sha256);
@@ -51,6 +56,7 @@ std::string electronpass::Crypto::encrypt(const std::string &plain_text, bool &s
     byte signature[CryptoPP::SHA256::DIGESTSIZE];
     hash_filter.Get(signature, static_cast<size_t >(CryptoPP::SHA256::DIGESTSIZE));
 
+    // Convert encrypted_bytes to Base64, add signature and IV.
     CryptoPP::Base64Encoder encoder(nullptr, false);
     encoder.Put(encrypted_bytes, encrypted_size);
     encoder.Put(signature, static_cast<size_t >(CryptoPP::SHA256::DIGESTSIZE));
@@ -64,11 +70,17 @@ std::string electronpass::Crypto::encrypt(const std::string &plain_text, bool &s
 
     delete[] encrypted_bytes;
 
-    success = true;
     return encrypted_message;
 }
 
+// Function used for backwards compatibility.
+std::string electronpass::Crypto::encrypt(const std::string &plain_text, bool &success) const {
+    success = true;
+    return encrypt(plain_text);
+}
+
 std::string electronpass::Crypto::decrypt(const std::string &base64_cipher_text, bool &success) const {
+    // Decode cipher_text from Base64.
     CryptoPP::Base64Decoder decoder;
     decoder.Put((byte *) base64_cipher_text.data(), base64_cipher_text.size());
     decoder.MessageEnd();
@@ -77,23 +89,30 @@ std::string electronpass::Crypto::decrypt(const std::string &base64_cipher_text,
     auto message_bytes = new byte[message_byte_size];
     decoder.Get(message_bytes, message_byte_size);
 
+    // Calculate where starts signature and IV.
     unsigned long iv_beginning = message_byte_size - AES_BLOCKSIZE;
-    unsigned long signature_beginning = message_byte_size - AES_BLOCKSIZE - CryptoPP::SHA256::DIGESTSIZE;
+    unsigned long signature_beginning = iv_beginning - CryptoPP::SHA256::DIGESTSIZE;
 
+    // Retrieve IV
     byte iv[AES_BLOCKSIZE];
-    for (unsigned long i = iv_beginning; i < iv_beginning + AES_BLOCKSIZE; ++i) {
-        iv[i - iv_beginning] = message_bytes[i];
+    for (unsigned long i = 0; i < AES_BLOCKSIZE; ++i) {
+        iv[i] = message_bytes[i + iv_beginning];
     }
 
+    // Retrieve signature
     byte signature[CryptoPP::SHA256::DIGESTSIZE];
-    for (unsigned long i = signature_beginning; i < signature_beginning + CryptoPP::SHA256::DIGESTSIZE; ++i) {
-        signature[i - signature_beginning] = message_bytes[i];
+    for (unsigned long i = 0; i < CryptoPP::SHA256::DIGESTSIZE; ++i) {
+        signature[i] = message_bytes[i + signature_beginning];
     }
 
+    // Retrieve cipher text
     unsigned long encrypted_bytes_size = signature_beginning;
     auto encrypted_bytes = new byte[encrypted_bytes_size];
     for (unsigned long i = 0; i < signature_beginning; ++i) encrypted_bytes[i] = message_bytes[i];
 
+    delete[] message_bytes;
+
+    // Generate signature
     byte generated_signature[CryptoPP::SHA256::DIGESTSIZE];
 
     CryptoPP::SHA256 sha256;
@@ -104,6 +123,7 @@ std::string electronpass::Crypto::decrypt(const std::string &base64_cipher_text,
 
     hash_filter.Get(generated_signature, static_cast<size_t >(CryptoPP::SHA256::DIGESTSIZE));
 
+    // Check if signatures are equal.
     bool signatures_match = true;
     for (int i = 0; i < CryptoPP::SHA256::DIGESTSIZE; ++i) {
         if (signature[i] != generated_signature[i]) {
@@ -112,14 +132,14 @@ std::string electronpass::Crypto::decrypt(const std::string &base64_cipher_text,
         }
     }
 
-    delete[] message_bytes;
-
+    // If signatures do not match -> key is incorrect
     if (!signatures_match) {
         delete[] encrypted_bytes;
         success = false;
         return "";
     }
 
+    // Actual decryption.
     std::string decrypted_string;
     CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption decryption;
     decryption.SetKeyWithIV(key, static_cast<size_t >(KEY_SIZE), iv);
@@ -137,6 +157,7 @@ std::string electronpass::Crypto::decrypt(const std::string &base64_cipher_text,
 }
 
 std::string electronpass::Crypto::generate_uuid() {
+    // Generates random UUID.
     const unsigned int uuid_size = 24; // Bytes
     unsigned char uuid[uuid_size];
     CryptoPP::AutoSeededRandomPool random_pool;
